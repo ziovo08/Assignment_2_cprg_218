6e2fa6da72a58a488da4d3ba10c0394cb7bf9fa5
ziovo08 <zoe.lachica@edu.sait.ca>
2024-03-21 11:24:46 -0600

    Accordion java and CSS

--- java/java.js
+++ java/java.js
@@ -1,25 +1,22 @@
 console.log("JavaScript is linked and running!");
 
 
+
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Select all accordion items
     const accordions = document.querySelectorAll('.accordion h3');
 
     accordions.forEach(acc => {
         acc.addEventListener('click', function() {
-            // Toggle the "active" class on the clicked header
             this.classList.toggle('active');
-
-            // Get the next sibling element (the panel)
             var panel = this.nextElementSibling;
-
-            // Toggle the panel's display
-            if (panel.style.display === 'block') {
-                panel.style.display = 'none';
+            
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
             } else {
-                panel.style.display = 'block';
+                panel.style.maxHeight = panel.scrollHeight + "px";
             }
         });
     });
 });
+
 
