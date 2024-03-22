e0eca8b4a5e6cfe324f70e67a127218bab0aef64
ziovo08 <zoe.lachica@edu.sait.ca>
2024-03-21 23:18:07 -0600

    feedback implement

--- java/java.js
+++ java/java.js
@@ -1,14 +1,12 @@
 console.log("JavaScript is linked and running!");
 
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
     const accordions = document.querySelectorAll('.accordion h3');
 
     accordions.forEach(acc => {
-        acc.addEventListener('click', function() {
-            this.classList.toggle('active');
-            var panel = this.nextElementSibling;
+        acc.addEventListener('click', () => {
+            acc.classList.toggle('active');
+            const panel = acc.nextElementSibling;
             
             if (panel.style.maxHeight) {
                 panel.style.maxHeight = null;
@@ -19,5 +17,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 });
 
-/* Icons */
-
+/* Attributions */
+/* Accordion Code derived from w3schools https://www.w3schools.com/w3css/w3css_accordions.asp */
+/* Reference to ES6 https://www.w3schools.com/js/js_es6.asp */
